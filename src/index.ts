2d05e162f9647115f70a76e4514478ce65a9353e
maxwellium <x>
2019-03-05 12:47:36 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { Http2ServerResponse } from 'http2';
+
+
+export function http2jsonSend( res: Http2ServerResponse, data: any, statusCode = 200 ) {
+  return new Promise( ( resolve, reject ) => {
+
+    const str = JSON.stringify( data );
+    const payload = Buffer.from( str );
+
+    res.on( 'error', reject );
+    res.statusCode = statusCode;
+
+    res.setHeader( 'Content-Type', 'application/json' );
+    res.setHeader( 'Content-Length', payload.length );
+
+    res.end( payload, resolve );
+
+  } );
+}
